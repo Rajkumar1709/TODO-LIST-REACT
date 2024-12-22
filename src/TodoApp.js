d7f440fc2ea0184e0c79154dc2c4cb95e89e042a
Rajkumar <rajkumarvanjaku1709@gmail.com>
2024-12-23 00:08:26 +0530

    intial commit

---
+++ src/TodoApp.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, { id: Date.now(), text: todo, completed: false }]);
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(todos.map(todo => 
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    ));
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  return (
+    <div className="todo-app">
+      <h1>TODO Application</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </div>
+  );
+};
+
+export default TodoApp;
